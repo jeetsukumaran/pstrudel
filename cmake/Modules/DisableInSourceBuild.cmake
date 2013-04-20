c096112eab8c807cbf2f1e465da94601663ed06d
Jeet Sukumaran <jeetsukumaran@gmail.com>
2013-04-20 09:56:51 -0400

    disable in-source

--- cmake/Modules/DisableInSourceBuild.cmake
+++ cmake/Modules/DisableInSourceBuild.cmake
@@ -1,11 +1,11 @@
-IF( PROJECT_SOURCE_DIR STREQUAL PROJECT_BINARY_DIR AND NOT MSVC_IDE )
+IF( CMAKE_SOURCE_DIR STREQUAL CMAKE_BINARY_DIR AND NOT MSVC_IDE )
   MESSAGE(FATAL_ERROR
 "
 The build directory must be different from the main project source "
-"directory. Please create a directory such as '${PROJECT_SOURCE_DIR}/build', "
+"directory. Please create a directory such as '${CMAKE_SOURCE_DIR}/build', "
 "and run CMake from there, passing the path to this source directory as "
 "the path argument. E.g.:
-  $ cd ${PROJECT_SOURCE_DIR}
+  $ cd ${CMAKE_SOURCE_DIR}
   $ mkdir build
   $ cd build
   $ cmake .. && make && sudo make install
