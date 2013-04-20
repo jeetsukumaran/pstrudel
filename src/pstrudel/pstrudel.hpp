feb3f5a597e3d9229df52d7755746ff04c287c16
Jeet Sukumaran <jeetsukumaran@gmail.com>
2013-04-20 02:48:12 -0400

    tweaks to revision reporting

--- src/pstrudel/pstrudel.hpp
+++ src/pstrudel/pstrudel.hpp
@@ -9,10 +9,10 @@ namespace pstrudel {
 std::string get_program_identification(const std::string& program_name) {
     std::ostringstream s;
     s << program_name;
-#if defined(PROJECT_SOURCE_DESC)
-    s << " (" PROJECT_SOURCE_DESC << ") ";
+#if defined(PROJECT_SOURCE_IDENTIFIER)
+    s << " (" PROJECT_SOURCE_IDENTIFIER << ")";
 #else
-    s << " (" << __DATE__ << ")";
+    s << " (" << __DATE__ << " " << __TIME__ ")";
 #endif
     return  s.str();
 }
