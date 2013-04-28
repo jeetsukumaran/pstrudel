c770c9a6f31252a3e7d60dc2f255f062bd628090
Jeet Sukumaran <jeetsukumaran@gmail.com>
2013-04-28 04:25:58 -0400

    untested: profile refactoring in progress

--- src/pstrudel/profile.cpp
+++ src/pstrudel/profile.cpp
@@ -1,172 +1,277 @@
+#include <algorithm>
 #include <colugo/utility.hpp>
 #include "profile.hpp"
 
 namespace pstrudel {
 
-Profile::Profile(Profile::InterpolationMethod interpolation_method)
-        : interpolation_method_(interpolation_method) {
-}
+//////////////////////////////////////////////////////////////////////////////
+// Profile (public)
 
-void Profile::build_subprofiles() {
-    for (auto & raw_data_set : this->raw_data_) {
-        const std::string& raw_data_id = raw_data_set.first;
-        unsigned long num_interpolated_points = this->num_interpolated_points_[raw_data_id];
-        SubprofileDataType & raw_data_vector = raw_data_set.second;
-
-        this->subprofiles_[raw_data_id] = SubprofileDataType();
-        auto & subprofile = this->subprofiles_[raw_data_id];
-
-        unsigned long raw_data_size = raw_data_vector.size();
-        COLUGO_ASSERT(raw_data_size > 1);
-        if (num_interpolated_points < raw_data_size) {
-            colugo::colugo_abort("Error building subprofile '",
-                    raw_data_id,
-                    "': Number of requested interpolated points (",
-                    num_interpolated_points,
-                    ") less than raw data size (",
-                    raw_data_size,
-                    ")");
-        }
-
-        unsigned long default_bin_size = static_cast<unsigned long>(num_interpolated_points / (raw_data_size));
-        // if (default_bin_size <= 0) {
-        //     std::cerr << "*************\n";
-        //     std::cerr << num_interpolated_points << "/" << raw_data_size << " = " << default_bin_size << std::endl;
-        //     std::cerr << "*************\n";
-        // }
-        COLUGO_ASSERT(default_bin_size > 0);
-        std::vector<unsigned long> bin_sizes(raw_data_size, default_bin_size);
-
-        // due to rounding error, default bin size may not be enough
-        // this hacky algorithm adds additional points to bins to make up the
-        // difference, trying to distribute the additional points along the
-        // length of the line
-        unsigned long diff = num_interpolated_points - (default_bin_size * (raw_data_size));
-        if (diff > 0) {
-            double dv = static_cast<double>(diff) / (raw_data_size);
-            double cv = 0;
-            for (auto & bin : bin_sizes) {
-                if (diff <= 1) {
-                    break;
-                }
-                cv += dv;
-                if (cv >= 1) {
-                    bin += 1;
-                    diff -= 1;
-                    cv = cv - 1.0;
-                }
-            }
-        }
-
-        subprofile.reserve(num_interpolated_points + 1);
-        if (this->interpolation_method_ == Profile::InterpolationMethod::STAIRCASE) {
-            for (auto & rd : raw_data_vector) {
-                this->interpolate_flat(subprofile, rd, default_bin_size);
-            }
-        } else {
-            this->interpolate_linear(subprofile, 0, 0, raw_data_vector[0], bin_sizes[0]);
-            for (unsigned long bin_idx=0; bin_idx < raw_data_size - 1; ++bin_idx) {
-                this->interpolate_linear(subprofile, bin_idx, raw_data_vector[bin_idx], raw_data_vector[bin_idx+1], bin_sizes[bin_idx], num_interpolated_points);
-            }
-            // this->interpolate_linear(subprofile, raw_data_size-1, raw_data_vector[raw_data_size-2], raw_data_vector.back(), bin_size);
-        }
-        subprofile.push_back(raw_data_vector.back());
-    }
-}
-
-const Profile::SubprofileDataType& Profile::get_subprofile(const std::string& subprofile_id) {
-    COLUGO_ASSERT(this->subprofiles_.find(subprofile_id) != this->subprofiles_.end());
-    return this->subprofiles_[subprofile_id];
-}
-
-double Profile::calc_subprofile_distance(const Profile& other, const std::string& subprofile_id) const {
-    return std::sqrt(this->sum_of_squares(other, subprofile_id));
-}
-
-double Profile::calc_distance(const Profile& other) const {
-    COLUGO_ASSERT(this->subprofiles_.size() == other.subprofiles_.size());
-    double ss = 0.0;
-    for (auto & subprofile_pair : this->subprofiles_) {
-        const std::string& subprofile_id = subprofile_pair.first;
-        ss += this->sum_of_squares(other, subprofile_id);
-    }
-    return std::sqrt(ss);
+Profile::Profile(unsigned long fixed_size,
+        Profile::InterpolationMethod interpolation_method)
+    : fixed_size_(fixed_size)
+      , interpolation_method_(interpolation_method) {
 }
 
 void Profile::clear() {
     this->raw_data_.clear();
-    this->subprofiles_.clear();
+    this->interpolated_profiles_.clear();
 }
 
-void Profile:: poll_max_num_interpolated_points(std::map<const std::string, unsigned long>& num_interpolated_points) {
-    for (auto & npp_iter : this->num_interpolated_points_) {
-        if (this->num_interpolated_points_[npp_iter.first] > num_interpolated_points[npp_iter.first]) {
-            num_interpolated_points[npp_iter.first] = this->num_interpolated_points_[npp_iter.first];
+double Profile::get_distance(Profile & other) {
+    unsigned long profile_size = 0;
+    if (this->fixed_size_ > 0 && other.fixed_size_ > 0) {
+        // fixed size profiles
+        if (this->fixed_size_ != other.fixed_size_) {
+            colugo::colugo_abort("Comparing two profiles locked to different sizes: ",
+                    this->fixed_size_, " and ", other.fixed_size_);
+            exit(1);
+        }
+        profile_size = this->fixed_size_;
+    } else if (this->fixed_size_ == 0 && other.fixed_size_ > 0) {
+        if (other.fixed_size_ < this->raw_data_.size()) {
+            colugo::colugo_abort("Cannot interpolate points in current profile:",
+                    " raw data size is ", this->raw_data_.size(),
+                    " but comparison requires ", other.fixed_size_,
+                    " because other profile is locked to fixed size");
+            exit(1);
+        }
+        profile_size = other.fixed_size_;
+    } else if (this->fixed_size_ > 0 && other.fixed_size_ == 0) {
+        if (this->fixed_size_ < other.raw_data_.size()) {
+            colugo::colugo_abort("Cannot interpolate points in other profile:",
+                    " other raw data size is ", other.raw_data_.size(),
+                    " but comparison requires ", this->fixed_size_,
+                    " because this profile is locked to fixed size");
+            exit(1);
+        }
+        profile_size = this->fixed_size_;
+    } else {
+        profile_size = std::max(this->raw_data_.size(), other.raw_data_.size());
+    }
+    auto v1_iter = this->interpolated_profiles_.find(profile_size);
+    if (v1_iter == this->interpolated_profiles_.end()) {
+        this->build_interpolated_profile(profile_size);
+    }
+    auto v2_iter = other.interpolated_profiles_.find(profile_size);
+    if (v2_iter == other.interpolated_profiles_.end()) {
+        other.build_interpolated_profile(profile_size);
+    }
+    auto & v1 = this->interpolated_profiles_[profile_size];
+    auto & v2 = other.interpolated_profiles_[profile_size];
+    return Profile::calc_euclidean_distance(v1, v2);
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+// Profile (protected/private)
+
+void Profile::build_interpolated_profile(
+        unsigned long profile_size) {
+    std::vector<double> & interpolated_profile = this->interpolated_profiles_[profile_size];
+    interpolated_profile.clear();
+    unsigned long raw_data_size = raw_data_vector.size();
+    COLUGO_ASSERT(raw_data_size > 1);
+    if (profile_size < raw_data_size) {
+        colugo::colugo_abort("Error interpolating points in profile ",
+                ": Number of requested interpolated points (",
+                profile_size,
+                ") less than raw data size (",
+                raw_data_size,
+                ")");
+    }
+    unsigned long default_bin_size = static_cast<unsigned long>(profile_size / (raw_data_size));
+    COLUGO_ASSERT(default_bin_size > 0);
+    std::vector<unsigned long> bin_sizes(raw_data_size, default_bin_size);
+
+    // due to rounding error, default bin size may not be enough
+    // this hacky algorithm adds additional points to bins to make up the
+    // difference, trying to distribute the additional points along the
+    // length of the line
+    unsigned long diff = profile_size - (default_bin_size * (raw_data_size));
+    if (diff > 0) {
+        double dv = static_cast<double>(diff) / (raw_data_size);
+        double cv = 0;
+        for (auto & bin : bin_sizes) {
+            if (diff <= 1) {
+                break;
+            }
+            cv += dv;
+            if (cv >= 1) {
+                bin += 1;
+                diff -= 1;
+                cv = cv - 1.0;
+            }
         }
     }
-}
 
-void Profile:: set_num_interpolated_points(std::map<const std::string, unsigned long>& num_interpolated_points) {
-    for (auto & npp_iter : this->num_interpolated_points_) {
-        this->num_interpolated_points_[npp_iter.first] = num_interpolated_points[npp_iter.first];
+    interpolated_profile.reserve(profile_size + 1);
+    if (this->interpolation_method_ == Profile::InterpolationMethod::STAIRCASE) {
+        for (auto & rd : this->raw_data_) {
+            this->interpolate_flat(interpolated_profile, rd, default_bin_size);
+        }
+    } else {
+        this->interpolate_linear(interpolated_profile, 0, 0, this->raw_data_[0], bin_sizes[0]);
+        for (unsigned long bin_idx=0; bin_idx < raw_data_size - 1; ++bin_idx) {
+            this->interpolate_linear(interpolated_profile, bin_idx, this->raw_data_[bin_idx], this->raw_data_[bin_idx+1], bin_sizes[bin_idx], profile_size);
+        }
     }
+    interpolated_profile.push_back(this->raw_data_.back());
 }
 
-void Profile::interpolate_flat(SubprofileDataType& subprofile, double value, unsigned long num_points) {
+void Profile::interpolate_flat(std::vector<double> & interpolated_profile, double value, unsigned long num_points) {
     while (num_points > 0) {
-        subprofile.push_back(value);
+        interpolated_profile.push_back(value);
         --num_points;
     }
 }
-void Profile::interpolate_linear(SubprofileDataType& subprofile, double x1, double y1, double y2, unsigned long num_points, unsigned long max_points) {
+void Profile::interpolate_linear(std::vector<double> & interpolated_profile, double x1, double y1, double y2, unsigned long num_points, unsigned long max_points) {
     // pstrudel_print(std::cout, "\n---\n", "x1 = ", x1, ", y1 = ", y1, ", y2 = ", y2, "; n=", num_points, "\n");
     COLUGO_ASSERT(num_points > 0);
     double slope = (y2 - y1)/num_points;
     COLUGO_ASSERT(slope >= 0);
     for (unsigned long xi = 0; xi < num_points; ++xi) {
-        if (max_points > 0 && subprofile.size() >= max_points) {
+        if (max_points > 0 && interpolated_profile.size() >= max_points) {
             return;
         }
-        subprofile.push_back(slope * xi + y1);
-        // pstrudel_print(std::cout, xi, ": ", subprofile.back(), "\n");
+        interpolated_profile.push_back(slope * xi + y1);
+        // pstrudel_print(std::cout, xi, ": ", interpolated_profile.back(), "\n");
     }
 }
 
-double Profile::sum_of_squares(const Profile& other, const std::string& subprofile_id) const {
-    auto sp_pair1 = this->subprofiles_.find(subprofile_id);
-    auto sp_pair2 = other.subprofiles_.find(subprofile_id);
-    if (sp_pair1 == this->subprofiles_.end()) {
-        colugo::colugo_abort("Subprofile '", subprofile_id, "' not found");
-        exit(1);
+double Profile::calc_euclidean_distance(std::vector<double> & v1, std::vector<double> & v2) {
+    unsigned long v1_size = v1.size();
+    unsigned long v2_size = v2.size();
+    unsigned long v1_idx = 0;
+    unsigned long v2_idx = 0;
+    if (v1_size > v2_size) {
+        v1_idx = v1_size - v2_size;
     }
-    if (sp_pair2 == other.subprofiles_.end()) {
-        colugo::colugo_abort("Subprofile '", subprofile_id, "' not found");
-        exit(1);
-    }
-    auto & subprofile1 = sp_pair1->second;
-    auto & subprofile2 = sp_pair2->second;
-    unsigned long sp1_size = subprofile1.size();
-    unsigned long sp2_size = subprofile2.size();
-    unsigned long sp1_idx = 0;
-    unsigned long sp2_idx = 0;
-    if (sp1_size > sp2_size) {
-        sp1_idx = sp1_size - sp2_size;
-    }
-    if (sp2_size > sp1_size) {
-        sp2_idx = sp2_size - sp1_size;
+    if (v2_size > v1_size) {
+        v2_idx = v2_size - v1_size;
     }
     double ss = 0.0;
-    unsigned long num_comparisons = 0;
-    while (sp1_idx < sp1_size && sp2_idx < sp2_size) {
-        ss += std::pow(subprofile1[sp1_idx] - subprofile2[sp2_idx], 2);
-        ++sp1_idx;
-        ++sp2_idx;
-        ++num_comparisons;
+    while (v1_idx < v1_size && v2_idx < v2_size) {
+        ss += std::pow(v1[v1_idx] - v2[v2_idx], 2);
+        ++v1_idx;
+        ++v2_idx;
     }
-    // std::fstream x;
-    // x.open("x.txt", std::fstream::out | std::fstream::app);
-    // x << num_comparisons << std::endl;
-    return ss;
+    return std::sqrt(ss);
 }
+
+
+
+
+
+
+
+
+
+
+// void Profile::build_subprofiles() {
+//     for (auto & raw_data_set : this->raw_data_) {
+//         const std::string& raw_data_id = raw_data_set.first;
+//         unsigned long num_interpolated_points = this->num_interpolated_points_[raw_data_id];
+//         std::vector<double> & raw_data_vector = raw_data_set.second;
+
+//         this->subprofiles_[raw_data_id] = std::vector<double>();
+//         auto & subprofile = this->subprofiles_[raw_data_id];
+
+//         unsigned long raw_data_size = raw_data_vector.size();
+//         COLUGO_ASSERT(raw_data_size > 1);
+//         if (num_interpolated_points < raw_data_size) {
+//             colugo::colugo_abort("Error building subprofile '",
+//                     raw_data_id,
+//                     "': Number of requested interpolated points (",
+//                     num_interpolated_points,
+//                     ") less than raw data size (",
+//                     raw_data_size,
+//                     ")");
+//         }
+
+//         unsigned long default_bin_size = static_cast<unsigned long>(num_interpolated_points / (raw_data_size));
+//         // if (default_bin_size <= 0) {
+//         //     std::cerr << "*************\n";
+//         //     std::cerr << num_interpolated_points << "/" << raw_data_size << " = " << default_bin_size << std::endl;
+//         //     std::cerr << "*************\n";
+//         // }
+//         COLUGO_ASSERT(default_bin_size > 0);
+//         std::vector<unsigned long> bin_sizes(raw_data_size, default_bin_size);
+
+//         // due to rounding error, default bin size may not be enough
+//         // this hacky algorithm adds additional points to bins to make up the
+//         // difference, trying to distribute the additional points along the
+//         // length of the line
+//         unsigned long diff = num_interpolated_points - (default_bin_size * (raw_data_size));
+//         if (diff > 0) {
+//             double dv = static_cast<double>(diff) / (raw_data_size);
+//             double cv = 0;
+//             for (auto & bin : bin_sizes) {
+//                 if (diff <= 1) {
+//                     break;
+//                 }
+//                 cv += dv;
+//                 if (cv >= 1) {
+//                     bin += 1;
+//                     diff -= 1;
+//                     cv = cv - 1.0;
+//                 }
+//             }
+//         }
+
+//         subprofile.reserve(num_interpolated_points + 1);
+//         if (this->interpolation_method_ == Profile::InterpolationMethod::STAIRCASE) {
+//             for (auto & rd : raw_data_vector) {
+//                 this->interpolate_flat(subprofile, rd, default_bin_size);
+//             }
+//         } else {
+//             this->interpolate_linear(subprofile, 0, 0, raw_data_vector[0], bin_sizes[0]);
+//             for (unsigned long bin_idx=0; bin_idx < raw_data_size - 1; ++bin_idx) {
+//                 this->interpolate_linear(subprofile, bin_idx, raw_data_vector[bin_idx], raw_data_vector[bin_idx+1], bin_sizes[bin_idx], num_interpolated_points);
+//             }
+//             // this->interpolate_linear(subprofile, raw_data_size-1, raw_data_vector[raw_data_size-2], raw_data_vector.back(), bin_size);
+//         }
+//         subprofile.push_back(raw_data_vector.back());
+//     }
+// }
+
+// const Profile::std::vector<double>& Profile::get_subprofile(const std::string& subprofile_id) {
+//     COLUGO_ASSERT(this->subprofiles_.find(subprofile_id) != this->subprofiles_.end());
+//     return this->subprofiles_[subprofile_id];
+// }
+
+// double Profile::calc_subprofile_distance(const Profile& other, const std::string& subprofile_id) const {
+//     return std::sqrt(this->sum_of_squares(other, subprofile_id));
+// }
+
+// double Profile::calc_distance(const Profile& other) const {
+//     COLUGO_ASSERT(this->subprofiles_.size() == other.subprofiles_.size());
+//     double ss = 0.0;
+//     for (auto & subprofile_pair : this->subprofiles_) {
+//         const std::string& subprofile_id = subprofile_pair.first;
+//         ss += this->sum_of_squares(other, subprofile_id);
+//     }
+//     return std::sqrt(ss);
+// }
+
+// void Profile::clear() {
+//     this->raw_data_.clear();
+//     this->subprofiles_.clear();
+// }
+
+// void Profile:: poll_max_num_interpolated_points(std::map<const std::string, unsigned long>& num_interpolated_points) {
+//     for (auto & npp_iter : this->num_interpolated_points_) {
+//         if (this->num_interpolated_points_[npp_iter.first] > num_interpolated_points[npp_iter.first]) {
+//             num_interpolated_points[npp_iter.first] = this->num_interpolated_points_[npp_iter.first];
+//         }
+//     }
+// }
+
+// void Profile:: set_num_interpolated_points(std::map<const std::string, unsigned long>& num_interpolated_points) {
+//     for (auto & npp_iter : this->num_interpolated_points_) {
+//         this->num_interpolated_points_[npp_iter.first] = num_interpolated_points[npp_iter.first];
+//     }
+// }
+
 
 } // namespace pstrudel
--- src/pstrudel/profile.hpp
+++ src/pstrudel/profile.hpp
@@ -1,72 +1,124 @@
 #ifndef PSTRUDEL_PROFILE_PROFILE_HPP
 #define PSTRUDEL_PROFILE_PROFILE_HPP
 
-#include <algorithm>
-#include <map>
-#include <fstream>
 #include <vector>
 #include <iostream>
 #include <cmath>
+#include <map>
 #include <iterator>
 
 namespace pstrudel {
 
 class Profile {
-
     public:
-        typedef std::vector<double>                 SubprofileDataType;
+
         enum class InterpolationMethod {
             STAIRCASE,
             PIECEWISE_LINEAR,
         };
 
     public:
-        Profile(Profile::InterpolationMethod interpolation_method=Profile::InterpolationMethod::PIECEWISE_LINEAR);
+
+        // if fixed_size == 0, then profiles are dynamically interpolated when comparing
+        // profiles to the maximum size of the profiles being compared
+        Profile(unsigned long fixed_size=0)
+                Profile::InterpolationMethod interpolation_method=Profile::InterpolationMethod::PIECEWISE_LINEAR);
 
         template <class iterator>
-        void add_subprofile_source(
-                const std::string& raw_data_id,
+        void add_raw_data(
                 iterator src_begin,
                 iterator src_end,
-                unsigned long num_interpolated_points=1000,
                 bool sort=true) {
-            this->raw_data_[raw_data_id] = SubprofileDataType(src_begin, src_end);
-            this->num_interpolated_points_[raw_data_id] = num_interpolated_points;
-            if (sort) {
-                auto & raw_data = this->raw_data_[raw_data_id];
-                std::sort(raw_data.begin(), raw_data.end());
+            this->interpolate_profiles_.clear();
+            this->raw_data_.insert(this->raw_data_.end(), src_begin, src_end);
+            if (this->fixed_size_ > 0 && this->raw_data.size() > this->fixed_size_) {
+                colugo::colugo_abort("Profile is fixed to ",
+                        this->fixed_size_,
+                        "but trying to add ",
+                        this->raw_data_.size(),
+                        " points as raw data");
             }
+            if (sort) {
+                std::sort(this->raw_data_.begin(), this->raw_data_.end());
+            }
+            // store raw data as profile for `n` points
+            auto & default_profile = this->interpolated_profiles_[this->raw_data_.size()];
+            default_profile.insert(this->raw_data_.end(), src_begin, src_end);
         }
 
-        template <class iterator>
-        void insert_subprofile(
-                const std::string& subprofile_id,
-                iterator src_begin,
-                iterator src_end) {
-            this->subprofiles_[subprofile_id] = SubprofileDataType(src_begin, src_end);
-        }
+        double get_distance(Profile & other);
 
-        void build_subprofiles();
-        const SubprofileDataType& get_subprofile(const std::string& subprofile_id);
-        double calc_subprofile_distance(const Profile& other, const std::string& subprofile_id) const;
-        double calc_distance(const Profile& other) const;
-        void clear();
-
-        void poll_max_num_interpolated_points(std::map<const std::string, unsigned long>& num_interpolated_points);
-        void set_num_interpolated_points(std::map<const std::string, unsigned long>& num_interpolated_points);
 
     private:
-        void interpolate_flat(SubprofileDataType& subprofile, double value, unsigned long num_points);
-        void interpolate_linear(SubprofileDataType& subprofile, double x1, double y1, double y2, unsigned long num_points, unsigned long max_points=0);
-        double sum_of_squares(const Profile& other, const std::string& subprofile_id) const;
+        void build_interpolated_profile(unsigned long profile_size);
+        static void interpolate_flat(std::vector<double> & subprofile, double value, unsigned long num_points);
+        static void interpolate_linear(std::vector<double> & subprofile, double x1, double y1, double y2, unsigned long num_points, unsigned long max_points=0);
+        static double calc_euclidean_distance(std::vector<double> & v1, std::vector<double> & v2);
 
     private:
-        Profile::InterpolationMethod                        interpolation_method_;
-        std::map<const std::string, SubprofileDataType>     raw_data_;
-        std::map<const std::string, unsigned long>          num_interpolated_points_;
-        std::map<const std::string, SubprofileDataType>     subprofiles_;
+        unsigned long                               fixed_size_;
+        Profile::InterpolationMethod                interpolation_method_;
+        std::vector<double>                         raw_data_;
+        std::map<unsigned long, std::vector<double> interpolated_profiles_;
 
 }; // Profile
+
+// class Profile {
+
+//     public:
+//         typedef std::vector<double>                 std::vector<double>;
+//         enum class InterpolationMethod {
+//             STAIRCASE,
+//             PIECEWISE_LINEAR,
+//         };
+
+//     public:
+//         Profile(Profile::InterpolationMethod interpolation_method=Profile::InterpolationMethod::PIECEWISE_LINEAR);
+
+//         template <class iterator>
+//         void add_subprofile_source(
+//                 const std::string& raw_data_id,
+//                 iterator src_begin,
+//                 iterator src_end,
+//                 unsigned long num_interpolated_points=1000,
+//                 bool sort=true) {
+//             this->raw_data_[raw_data_id] = std::vector<double>(src_begin, src_end);
+//             this->num_interpolated_points_[raw_data_id] = num_interpolated_points;
+//             if (sort) {
+//                 auto & raw_data = this->raw_data_[raw_data_id];
+//                 std::sort(raw_data.begin(), raw_data.end());
+//             }
+//         }
+
+//         template <class iterator>
+//         void insert_subprofile(
+//                 const std::string& subprofile_id,
+//                 iterator src_begin,
+//                 iterator src_end) {
+//             this->subprofiles_[subprofile_id] = std::vector<double>(src_begin, src_end);
+//         }
+
+//         void build_subprofiles();
+//         const std::vector<double>& get_subprofile(const std::string& subprofile_id);
+//         double calc_subprofile_distance(const Profile& other, const std::string& subprofile_id) const;
+//         double calc_distance(const Profile& other) const;
+//         void clear();
+
+//         void poll_max_num_interpolated_points(std::map<const std::string, unsigned long>& num_interpolated_points);
+//         void set_num_interpolated_points(std::map<const std::string, unsigned long>& num_interpolated_points);
+
+//     private:
+//         void interpolate_flat(std::vector<double>& subprofile, double value, unsigned long num_points);
+//         void interpolate_linear(std::vector<double>& subprofile, double x1, double y1, double y2, unsigned long num_points, unsigned long max_points=0);
+//         double sum_of_squares(const Profile& other, const std::string& subprofile_id) const;
+
+//     private:
+//         Profile::InterpolationMethod                        interpolation_method_;
+//         std::map<const std::string, std::vector<double>>     raw_data_;
+//         std::map<const std::string, unsigned long>          num_interpolated_points_;
+//         std::map<const std::string, std::vector<double>>     subprofiles_;
+
+// }; // Profile
 
 } // namespace pstrudel
 
