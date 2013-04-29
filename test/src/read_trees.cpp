6e7c11c38ac4f69580327b99fa162a25eaca29b1
Jeet Sukumaran <jeetsukumaran@gmail.com>
2013-04-29 02:43:25 -0400

    test update

--- test/src/read_trees.cpp
+++ test/src/read_trees.cpp
@@ -2,11 +2,21 @@
 #include <iostream>
 #include <pstrudel/distancetree.hpp>
 #include <pstrudel/dataio.hpp>
+#include "supportlib.hpp"
 
-#include <unistd.h>
-#include <stdio.h>
 int main() {
     typedef pstrudel::DistanceTree  TreeType;
     std::vector<TreeType>  trees;
     pstrudel::treeio::read_from_filepath(trees, "data/trees/basic-patterns/n06-rooted-patterns.nex", "nexus");
+    if (trees.size() != 6) {
+        return pstrudel::test::fail_test(__FILE__,
+                6, trees.size(), "Incorrect number of trees read from file");
+    }
+    for (auto & tree : trees) {
+        if (tree.get_num_tips() != 6) {
+            return pstrudel::test::fail_test(__FILE__,
+                    6, tree.get_num_tips(), "Incorrect number of tips recorded");
+        }
+    }
+    return EXIT_SUCCESS;
 }
--- test/src/supportlib.hpp
+++ test/src/supportlib.hpp
@@ -1,13 +1,52 @@
-#include <string>
-#include <unistd.h>
-#include <stdio.h>
-
 #ifndef PSTRUDEL_TEST_SUPPORT_LIB
 #define PSTRUDEL_TEST_SUPPORT_LIB
 
-namespace pstrudeltest {
+#include <vector>
+#include <iostream>
+#include <string>
 
+namespace pstrudel { namespace test {
 
-} // namespace pstrudeltest
+template <typename S>
+void log(S&) {}
+
+template <typename S, typename T>
+void log(S& stream, const T& arg1) {
+    stream << arg1;
+}
+
+template <typename S, typename T>
+void log(S& stream, const std::vector<T>& arg1) {
+    write_container(arg1, stream, ", ");
+    log(stream);
+}
+
+template <typename S, typename T, typename... Types>
+void log(S& stream, const std::vector<T>& arg1, const Types&... args) {
+    write_container(arg1, stream, ", ");
+    log(stream, args...);
+}
+
+template <typename S, typename T, typename... Types>
+void log(S& stream, const T& arg1, const Types&... args) {
+    stream << arg1;
+    log(stream, args...);
+}
+
+template <typename T, typename U, typename... Types>
+int fail_test(const std::string& test_name,
+        const T& expected,
+        const U& observed,
+        const Types&... args) {
+    std::cerr << "\n||[[ FAIL ]]]";
+    std::cerr << "\n||     Test: " << test_name;
+    log(std::cerr, "\n|| Expected: ", expected);
+    log(std::cerr, "\n|| Observed: ", observed);
+    log(std::cerr, "\n||  Remarks: ", args...);
+    std::cerr << std::endl;
+    return EXIT_FAILURE;
+}
+
+} } // namespace pstrudel::test
 
 #endif
