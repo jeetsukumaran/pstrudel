9d56142a53398a557bd543499adba1f027da0af3
Jeet Sukumaran <jeetsukumaran@gmail.com>
2013-05-22 16:13:32 -0400

    test actual distance calc

---
+++ test/scripts/calc-tree-ltt-distance.py
@@ -0,0 +1,102 @@
+#! /usr/bin/env python
+
+import os
+import sys
+import math
+import argparse
+import dendropy
+from dendropy import treecalc
+
+def euclidean_distance(v1, v2):
+    ss = 0.0
+    assert len(v1) == len(v2)
+    for idx1 in range(len(v1)):
+        ss += pow((v1[idx1] - v2[idx1]), 2)
+    return math.sqrt(ss)
+
+def preprocess_trees(trees, num_transects=None):
+    for tree in trees:
+        tree_length = tree.length()
+        num_leaves = len(tree.leaf_nodes())
+        max_leaf_distance_from_root = tree.minmax_leaf_distance_from_root()[1]
+        if not num_transects:
+            num_transects = (num_leaves - 1) * 10
+        offset_step = float(max_leaf_distance_from_root) / num_transects
+        transect_offsets = []
+        offset = offset_step
+        for transect_idx in range(num_transects):
+            transect_offsets.append(offset)
+            offset += offset_step
+        tree.lineage_accumulation_through_time = []
+        for tof in transect_offsets:
+            n = float(tree.num_lineages_at(tof))/num_leaves
+            tree.lineage_accumulation_through_time.append(n)
+        tree.lineage_splitting_times = []
+        tree.scaled_lineage_splitting_times = []
+        for node in tree.postorder_node_iter():
+            if tree_length > 0:
+                tree.lineage_splitting_times.append(node.root_distance)
+                tree.scaled_lineage_splitting_times.append(float(node.root_distance)/tree_length)
+            else:
+                tree.lineage_splitting_times.append(0.0)
+                tree.scaled_lineage_splitting_times.append(0.0)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("treefile",
+            type=str,
+            nargs="*",
+            help="path to tree file(s)")
+    parser.add_argument("-",
+            action="store_true",
+            default=False,
+            dest="trees_from_stdin",
+            help="read trees from standard input")
+    parser.add_argument("-f", "--format",
+            dest="schema",
+            type=str,
+            default="nexus",
+            choices=["nexus", "newick"],
+            help="input data format (default='%(default)s')")
+    parser.add_argument("-n", "--num-transects",
+            type=int,
+            default=None,
+            help="number of transects or slices on tree for lineage accumulation counts (default=%(default)s)")
+    parser.add_argument("-p", "--precision",
+            type=float,
+            default=1e-8,
+            help="numerical precision (default=%(default)s)")
+    parser.add_argument("-l", "--label",
+            type=str,
+            default="[TESTING]",
+            help="label for test")
+    parser.add_argument("--verbosity",
+            type=int,
+            default=0,
+            help="level of messaging")
+    args = parser.parse_args()
+
+    trees = dendropy.TreeList()
+    if args.trees_from_stdin:
+        trees.read_from_stream(sys.stdin, schema=args.schema)
+    for fpath in args.treefile:
+        trees.read_from_path(os.path.expanduser(os.path.expandvars(fpath)), schema=args.schema)
+    if len(trees) == 0:
+        sys.exit("Need to specify '-' option and pipe trees to standard input and/or specify paths to at least one tree file as argument")
+    preprocess_trees(trees)
+    for tidx1 in range(len(trees)):
+        for tidx2 in range(len(trees)):
+            sys.stdout.write("{}\t{}\t{:.22f}\t{:.22f}\t{:.22f}\n".format(
+                tidx1,
+                tidx2,
+                euclidean_distance(trees[tidx1].lineage_accumulation_through_time , trees[tidx2].lineage_accumulation_through_time) ,
+                euclidean_distance(trees[tidx1].lineage_splitting_times           , trees[tidx2].lineage_splitting_times          )  ,
+                euclidean_distance(trees[tidx1].scaled_lineage_splitting_times    , trees[tidx2].scaled_lineage_splitting_times   )
+                ))
+
+
+if __name__ == "__main__":
+    main()
+
+
+
---
+++ test/src/tree_ltt_distance.cpp
@@ -0,0 +1,99 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <colugo/subprocess.hpp>
+#include <pstrudel/distancetree.hpp>
+#include <pstrudel/dataio.hpp>
+#include "pstrudel_testing.hpp"
+
+std::string TEST_DIR;
+std::string CHECK_SCRIPT;
+
+int test_file(const std::string & test_data_filename, const std::string label) {
+    typedef pstrudel::DistanceTree  TreeType;
+    std::vector<TreeType>  trees;
+    std::string test_data_filepath = pstrudel::test::join_path(TEST_DIR, "data", "trees", "general", test_data_filename);
+    pstrudel::treeio::read_from_filepath(trees, test_data_filepath, "nexus");
+
+    std::map<unsigned long, std::map<unsigned long, double>> lc_dists;
+    std::map<unsigned long, std::map<unsigned long, double>> lst_dists;
+    std::map<unsigned long, std::map<unsigned long, double>> slst_dists;
+    std::set<std::pair<unsigned long, unsigned long>> comparisons;
+    for (unsigned tidx1 = 0; tidx1 < trees.size(); ++tidx1) {
+        for (unsigned tidx2 = 0; tidx2 < trees.size(); ++tidx2) {
+            auto & tree1 = trees[tidx1];
+            auto & tree2 = trees[tidx2];
+            lc_dists[tidx1][tidx2] = tree1.get_lineage_count_distance(tree2);
+            lst_dists[tidx1][tidx2] = tree1.get_lineage_splitting_time_distance(tree2);
+            slst_dists[tidx1][tidx2] = tree1.get_scaled_lineage_splitting_time_distance(tree2);
+            comparisons.insert(std::make_pair(tidx1, tidx2));
+        }
+    }
+    colugo::Subprocess ps({"python", CHECK_SCRIPT, "-f", "nexus", "-l", label, test_data_filepath});
+    int retcode = ps.wait();
+    auto ps_stdout = ps.get_stdout();
+    auto ps_stderr = ps.get_stderr();
+    if (retcode != 0) {
+        std::cerr << "(test '" << label << "' returned error: " << retcode << ")\n";
+        std::cerr << ps_stdout;
+        std::cerr << ps_stderr;
+        return 1;
+    }
+    int fails = 0;
+    unsigned long tidx1   = 0;
+    unsigned long tidx2   = 0;
+    double exp_lc_d   = 0.0;
+    double exp_lst_d  = 0.0;
+    double exp_slst_d = 0.0;
+    std::istringstream src(ps_stdout);
+    while (src.good()) {
+        src >> tidx1;
+        src >> tidx2;
+        src >> exp_lc_d;
+        src >> exp_lst_d;
+        src >> exp_slst_d;
+        fails += platypus::testing::compare_equal(
+                exp_lc_d,
+                lc_dists[tidx1][tidx2],
+                __FILE__,
+                __LINE__,
+                "Incorrect lineage accumulation through time profile distance for tree ", tidx1, " and tree ", tidx2);
+        fails += platypus::testing::compare_equal(
+                exp_lst_d,
+                lst_dists[tidx1][tidx2],
+                __FILE__,
+                __LINE__,
+                "Incorrect lineage splitting times profile distance for tree ", tidx1, " and tree ", tidx2);
+        fails += platypus::testing::compare_equal(
+                exp_slst_d,
+                slst_dists[tidx1][tidx2],
+                __FILE__,
+                __LINE__,
+                "Incorrect scaled lineage splitting times profile distance for tree ", tidx1, " and tree ", tidx2);
+        comparisons.erase(std::make_pair(tidx1, tidx2));
+    }
+    fails += platypus::testing::compare_equal(
+            0UL,
+            comparisons.size(),
+            __FILE__,
+            __LINE__,
+            "Not all trees checked");
+    return fails;
+}
+
+int test_dist1() {
+    int fails = 0;
+    return test_file("pythonidae.reference-trees.nexus", "pythonidae.reference-trees.nexus");
+}
+
+int main(int, const char * argv[]) {
+    TEST_DIR = pstrudel::test::get_test_dir(argv[0]);
+    CHECK_SCRIPT = pstrudel::test::join_path(TEST_DIR, "calc-tree-ltt-distance.py");
+    int fails = 0;
+    fails += test_dist1();
+    if (fails != 0) {
+        return EXIT_FAILURE;
+    } else {
+        return EXIT_SUCCESS;
+    }
+}
